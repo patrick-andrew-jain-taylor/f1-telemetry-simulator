533c2ee46d1b377beeec315686a7b7a4074c0c51
Patrick Jain-Taylor <patrick.andrew.taylor@gmail.com>
2021-09-28 11:17:56 -0400

    move: PacketHeader.py

--- f1_telemetry_2021/packets.py
+++ f1_telemetry_2021/packets.py
@@ -10,29 +10,7 @@ This package is based on the CodeMasters Forum post documenting the F1 2019 pack
 
 import ctypes
 import enum
-
-
-###########################################
-#                                         #
-#  __________  Packet Header  __________  #
-#                                         #
-###########################################
 from f1_telemetry_2021.PackedLittleEndianStructure import PackedLittleEndianStructure
-
-
-class PacketHeader(PackedLittleEndianStructure):
-    """The header for each of the UDP telemetry packets."""
-    _fields_ = [
-        ('packetFormat', ctypes.c_uint16),  # 2019
-        ('gameMajorVersion', ctypes.c_uint8),  # Game major version - "X.00"
-        ('gameMinorVersion', ctypes.c_uint8),  # Game minor version - "1.XX"
-        ('packetVersion', ctypes.c_uint8),  # Version of this packet type, all start from 1
-        ('packetId', ctypes.c_uint8),  # Identifier for the packet type, see below
-        ('sessionUID', ctypes.c_uint64),  # Unique identifier for the session
-        ('sessionTime', ctypes.c_float),  # Session timestamp
-        ('frameIdentifier', ctypes.c_uint32),  # Identifier for the frame the data was retrieved on
-        ('playerCarIndex', ctypes.c_uint8)  # Index of player's car in the array
-    ]
 
 
 @enum.unique
