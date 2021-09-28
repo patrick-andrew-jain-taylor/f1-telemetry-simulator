4328fd51d876cf7b3a378ca26072c102a1c176e7
Patrick Jain-Taylor <patrick.andrew.taylor@gmail.com>
2021-09-28 11:29:41 -0400

    move: PacketLapDataV1.py

--- f1_telemetry_2021/__init__.py
+++ f1_telemetry_2021/__init__.py
@@ -12,22 +12,12 @@ import ctypes
 import enum
 from f1_telemetry_2021.PackedLittleEndianStructure import PackedLittleEndianStructure
 from f1_telemetry_2021.PacketHeader import PacketHeader
+from f1_telemetry_2021.lap_data_packet.LapDataV1 import LapDataV1
+from f1_telemetry_2021.lap_data_packet.PacketLapDataV1 import PacketLapDataV1
 from f1_telemetry_2021.motion_packet.CarMotionDataV1 import CarMotionDataV1
 from f1_telemetry_2021.motion_packet.PacketMotionDataV1 import PacketMotionDataV1
 from f1_telemetry_2021.session_packet.MarshalZoneV1 import MarshalZoneV1
-
-
-class PacketLapDataV1(PackedLittleEndianStructure):
-    """The lap data packet gives details of all the cars in the session.
-
-    Frequency: Rate as specified in menus
-    Size: 843 bytes
-    Version: 1
-    """
-    _fields_ = [
-        ('header', PacketHeader),  # Header
-        ('lapData', LapDataV1 * 20)  # Lap data for all cars on track
-    ]
+from f1_telemetry_2021.session_packet.PacketSessionDataV1 import PacketSessionDataV1
 
 
 ########################################################
---
+++ f1_telemetry_2021/lap_data_packet/PacketLapDataV1.py
@@ -0,0 +1,14 @@
+from f1_telemetry_2021 import PackedLittleEndianStructure, PacketHeader, LapDataV1
+
+
+class PacketLapDataV1(PackedLittleEndianStructure):
+    """The lap data packet gives details of all the cars in the session.
+
+    Frequency: Rate as specified in menus
+    Size: 843 bytes
+    Version: 1
+    """
+    _fields_ = [
+        ('header', PacketHeader),  # Header
+        ('lapData', LapDataV1 * 20)  # Lap data for all cars on track
+    ]
