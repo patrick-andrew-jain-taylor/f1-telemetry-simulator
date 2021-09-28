267fa7de218e5213c465492c858b4db6d80288ee
Patrick Jain-Taylor <patrick.andrew.taylor@gmail.com>
2021-09-28 11:57:29 -0400

    change: PacketHeader.py

--- f1_telemetry_2021/PacketHeader.py
+++ f1_telemetry_2021/PacketHeader.py
@@ -6,7 +6,7 @@ from f1_telemetry_2019.PackedLittleEndianStructure import PackedLittleEndianStru
 class PacketHeader(PackedLittleEndianStructure):
     """The header for each of the UDP telemetry packets."""
     _fields_ = [
-        ('packetFormat', ctypes.c_uint16),  # 2019
+        ('packetFormat', ctypes.c_uint16),  # 2021
         ('gameMajorVersion', ctypes.c_uint8),  # Game major version - "X.00"
         ('gameMinorVersion', ctypes.c_uint8),  # Game minor version - "1.XX"
         ('packetVersion', ctypes.c_uint8),  # Version of this packet type, all start from 1
@@ -14,5 +14,7 @@ class PacketHeader(PackedLittleEndianStructure):
         ('sessionUID', ctypes.c_uint64),  # Unique identifier for the session
         ('sessionTime', ctypes.c_float),  # Session timestamp
         ('frameIdentifier', ctypes.c_uint32),  # Identifier for the frame the data was retrieved on
-        ('playerCarIndex', ctypes.c_uint8)  # Index of player's car in the array
+        ('playerCarIndex', ctypes.c_uint8),  # Index of player's car in the array
+        ('secondaryPlayerCarIndex', ctypes.c_uint8)  # Index of secondary player's car in the array,
+        # 255 if no second player
     ]
