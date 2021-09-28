536665c4b02a48b51cd20144f71aa838ad8e7fd6
Patrick Jain-Taylor <patrick.andrew.taylor@gmail.com>
2021-09-28 11:43:10 -0400

    move: CarSetupDataV1.py

--- f1_telemetry_2019/__init__.py
+++ f1_telemetry_2019/__init__.py
@@ -12,6 +12,7 @@ import ctypes
 import enum
 from f1_telemetry_2019.PackedLittleEndianStructure import PackedLittleEndianStructure
 from f1_telemetry_2019.PacketHeader import PacketHeader
+from f1_telemetry_2019.car_setups_packet.CarSetupDataV1 import CarSetupDataV1
 from f1_telemetry_2019.event_packet.PacketEventDataV1 import PacketEventDataV1
 from f1_telemetry_2019.lap_data_packet.LapDataV1 import LapDataV1
 from f1_telemetry_2019.lap_data_packet.PacketLapDataV1 import PacketLapDataV1
@@ -21,38 +22,6 @@ from f1_telemetry_2019.participants_packet.PacketParticipantsDataV1 import Packe
 from f1_telemetry_2019.participants_packet.ParticipantDataV1 import ParticipantDataV1
 from f1_telemetry_2019.session_packet.MarshalZoneV1 import MarshalZoneV1
 from f1_telemetry_2019.session_packet.PacketSessionDataV1 import PacketSessionDataV1
-
-
-#############################################################
-#                                                           #
-#  __________  Packet ID 5 : CAR SETUPS PACKET  __________  #
-#                                                           #
-#############################################################
-
-class CarSetupDataV1(PackedLittleEndianStructure):
-    """This type is used for the 20-element 'carSetups' array of the PacketCarSetupData_V1 type, defined below."""
-    _fields_ = [
-        ('frontWing', ctypes.c_uint8),  # Front wing aero
-        ('rearWing', ctypes.c_uint8),  # Rear wing aero
-        ('onThrottle', ctypes.c_uint8),  # Differential adjustment on throttle (percentage)
-        ('offThrottle', ctypes.c_uint8),  # Differential adjustment off throttle (percentage)
-        ('frontCamber', ctypes.c_float),  # Front camber angle (suspension geometry)
-        ('rearCamber', ctypes.c_float),  # Rear camber angle (suspension geometry)
-        ('frontToe', ctypes.c_float),  # Front toe angle (suspension geometry)
-        ('rearToe', ctypes.c_float),  # Rear toe angle (suspension geometry)
-        ('frontSuspension', ctypes.c_uint8),  # Front suspension
-        ('rearSuspension', ctypes.c_uint8),  # Rear suspension
-        ('frontAntiRollBar', ctypes.c_uint8),  # Front anti-roll bar
-        ('rearAntiRollBar', ctypes.c_uint8),  # Rear anti-roll bar
-        ('frontSuspensionHeight', ctypes.c_uint8),  # Front ride height
-        ('rearSuspensionHeight', ctypes.c_uint8),  # Rear ride height
-        ('brakePressure', ctypes.c_uint8),  # Brake pressure (percentage)
-        ('brakeBias', ctypes.c_uint8),  # Brake bias (percentage)
-        ('frontTyrePressure', ctypes.c_float),  # Front tyre pressure (PSI)
-        ('rearTyrePressure', ctypes.c_float),  # Rear tyre pressure (PSI)
-        ('ballast', ctypes.c_uint8),  # Ballast
-        ('fuelLoad', ctypes.c_float)  # Fuel load
-    ]
 
 
 class PacketCarSetupDataV1(PackedLittleEndianStructure):
---
+++ f1_telemetry_2019/car_setups_packet/CarSetupDataV1.py
@@ -0,0 +1,29 @@
+import ctypes
+
+from f1_telemetry_2019 import PackedLittleEndianStructure
+
+
+class CarSetupDataV1(PackedLittleEndianStructure):
+    """This type is used for the 20-element 'carSetups' array of the PacketCarSetupData_V1 type, defined below."""
+    _fields_ = [
+        ('frontWing', ctypes.c_uint8),  # Front wing aero
+        ('rearWing', ctypes.c_uint8),  # Rear wing aero
+        ('onThrottle', ctypes.c_uint8),  # Differential adjustment on throttle (percentage)
+        ('offThrottle', ctypes.c_uint8),  # Differential adjustment off throttle (percentage)
+        ('frontCamber', ctypes.c_float),  # Front camber angle (suspension geometry)
+        ('rearCamber', ctypes.c_float),  # Rear camber angle (suspension geometry)
+        ('frontToe', ctypes.c_float),  # Front toe angle (suspension geometry)
+        ('rearToe', ctypes.c_float),  # Rear toe angle (suspension geometry)
+        ('frontSuspension', ctypes.c_uint8),  # Front suspension
+        ('rearSuspension', ctypes.c_uint8),  # Rear suspension
+        ('frontAntiRollBar', ctypes.c_uint8),  # Front anti-roll bar
+        ('rearAntiRollBar', ctypes.c_uint8),  # Rear anti-roll bar
+        ('frontSuspensionHeight', ctypes.c_uint8),  # Front ride height
+        ('rearSuspensionHeight', ctypes.c_uint8),  # Rear ride height
+        ('brakePressure', ctypes.c_uint8),  # Brake pressure (percentage)
+        ('brakeBias', ctypes.c_uint8),  # Brake bias (percentage)
+        ('frontTyrePressure', ctypes.c_float),  # Front tyre pressure (PSI)
+        ('rearTyrePressure', ctypes.c_float),  # Rear tyre pressure (PSI)
+        ('ballast', ctypes.c_uint8),  # Ballast
+        ('fuelLoad', ctypes.c_float)  # Fuel load
+    ]
