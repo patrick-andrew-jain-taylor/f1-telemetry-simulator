84eb176445af4291cf545f83b4f958f205bff8e4
Patrick Jain-Taylor <patrick.andrew.taylor@gmail.com>
2021-09-28 11:23:21 -0400

    move: CarMotionDataV1.py

--- f1_telemetry_2021/__init__.py
+++ f1_telemetry_2021/__init__.py
@@ -11,36 +11,8 @@ This package is based on the CodeMasters Forum post documenting the F1 2019 pack
 import ctypes
 import enum
 from f1_telemetry_2021.PackedLittleEndianStructure import PackedLittleEndianStructure
-
-
-#########################################################
-#                                                       #
-#  __________  Packet ID 0 : MOTION PACKET  __________  #
-#                                                       #
-#########################################################
-
-class CarMotionDataV1(PackedLittleEndianStructure):
-    """This type is used for the 20-element 'carMotionData' array of the PacketMotionData_V1 type, defined below."""
-    _fields_ = [
-        ('worldPositionX', ctypes.c_float),  # World space X position
-        ('worldPositionY', ctypes.c_float),  # World space Y position
-        ('worldPositionZ', ctypes.c_float),  # World space Z position
-        ('worldVelocityX', ctypes.c_float),  # Velocity in world space X
-        ('worldVelocityY', ctypes.c_float),  # Velocity in world space Y
-        ('worldVelocityZ', ctypes.c_float),  # Velocity in world space Z
-        ('worldForwardDirX', ctypes.c_int16),  # World space forward X direction (normalised)
-        ('worldForwardDirY', ctypes.c_int16),  # World space forward Y direction (normalised)
-        ('worldForwardDirZ', ctypes.c_int16),  # World space forward Z direction (normalised)
-        ('worldRightDirX', ctypes.c_int16),  # World space right X direction (normalised)
-        ('worldRightDirY', ctypes.c_int16),  # World space right Y direction (normalised)
-        ('worldRightDirZ', ctypes.c_int16),  # World space right Z direction (normalised)
-        ('gForceLateral', ctypes.c_float),  # Lateral G-Force component
-        ('gForceLongitudinal', ctypes.c_float),  # Longitudinal G-Force component
-        ('gForceVertical', ctypes.c_float),  # Vertical G-Force component
-        ('yaw', ctypes.c_float),  # Yaw angle in radians
-        ('pitch', ctypes.c_float),  # Pitch angle in radians
-        ('roll', ctypes.c_float)  # Roll angle in radians
-    ]
+from f1_telemetry_2021.PacketHeader import PacketHeader
+from f1_telemetry_2021.motion_packet.CarMotionDataV1 import CarMotionDataV1
 
 
 class PacketMotionDataV1(PackedLittleEndianStructure):
